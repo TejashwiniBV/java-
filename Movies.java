82e0db354cb2fbc374127d08489acbd95d6da6d9
TejashwiniBV <tejashwinibv.xworkz@gmail.com>
2021-09-02 16:18:35 +0530

    added file

---
+++ Movies.java
@@ -0,0 +1,22 @@
+class Movies{
+
+
+static String Languge="Kannada";
+static String movieName="KGF";
+static String actor="Yash";
+static String cost="100crore";
+static long noOfActors=1500000L;
+
+public static void main(String a[]){
+
+
+System.out.println(Languge);
+System.out.println(movieName);
+System.out.println(actor);
+System.out.println(cost);
+System.out.println(noOfActors);
+
+
+
+}
+}
