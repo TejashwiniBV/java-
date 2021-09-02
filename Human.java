0518b51b0d9f55b564ffc3e2b1f6bfad79765cfe
TejashwiniBV <tejashwinibv.xworkz@gmail.com>
2021-09-02 15:57:09 +0530

    added java file

---
+++ Human.java
@@ -0,0 +1,13 @@
+class Human{
+
+public static void main(String a[]){
+
+String humanName="Akshara";
+int age=35;
+boolean isAlive=true;
+System.out.println(humanName);
+System.out.println(age);
+System.out.println(isAlive);
+
+}
+}
