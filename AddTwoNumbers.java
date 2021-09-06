1c12928f3877660a00661dd54b2af90c7efd3885
TejashwiniBV <89910795+TejashwiniBV@users.noreply.github.com>
2021-09-06 22:12:25 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,16 @@
+class AddTwoNumbers{
+
+public static void main(String a[]){
+add(68,43);
+add(618,3);
+add(78,413);
+add(95,45);
+add(128,63);
+add(53,83);
+
+}
+
+public static void add(int a, int b){
+System.out.println(a+b);
+}
+}
