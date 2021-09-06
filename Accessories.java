d286859881aa2dc0d5bf67bb13f9ae5cf669c361
TejashwiniBV <89910795+TejashwiniBV@users.noreply.github.com>
2021-09-06 22:11:56 +0530

    Add files via upload

---
+++ Accessories.java
@@ -0,0 +1,15 @@
+class Accessories{
+
+public static void main(String a[]){
+
+String accessoriesName="Watch";
+int  price=1150;
+boolean accessoriesAvailability=true;
+System.out.println(accessoriesName);
+System.out.println(price);
+System.out.println( accessoriesAvailability);
+
+
+
+}
+}
